652ece5b43dbeb719321feddc2cc8f475d9c9147
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-15 02:25:31 +0300

    feat(Tabs): implement tab components with dynamic content and structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+</script>
 <template>
   <div class="grid h-screen gap-8">
     <header class="flex items-start justify-between">
@@ -19,6 +21,39 @@
       <section>
         <div class="h-[360px] w-full bg-neutral-200"></div>
       </section>
+
+      <Tabs default-value="day" class="w-full">
+        <TabsList class="flex items-center gap-3">
+          <TabsTrigger class="px-7" value="day"> Day </TabsTrigger>
+          <TabsTrigger class="px-7" value="week"> Week </TabsTrigger>
+          <TabsTrigger class="px-7" value="month"> Month </TabsTrigger>
+          <TabsTrigger class="px-7" value="year"> Year </TabsTrigger>
+        </TabsList>
+        <TabsContent value="day">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          facilis velit optio temporibus officia reiciendis nam laboriosam harum
+          autem sed cumque quo alias vitae, corrupti non modi inventore delectus
+          molestiae!
+        </TabsContent>
+        <TabsContent value="week">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          facilis velit optio temporibus officia reiciendis nam laboriosam harum
+          autem sed cumque quo alias vitae, corrupti non modi inventore delectus
+          molestiae!</TabsContent
+        >
+        <TabsContent value="month">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          facilis velit optio temporibus officia reiciendis nam laboriosam harum
+          autem sed cumque quo alias vitae, corrupti non modi inventore delectus
+          molestiae!
+        </TabsContent>
+        <TabsContent value="year">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          facilis velit optio temporibus officia reiciendis nam laboriosam harum
+          autem sed cumque quo alias vitae, corrupti non modi inventore delectus
+          molestiae!</TabsContent
+        >
+      </Tabs>
     </main>
     <footer>
       <div class="flex flex-col items-center gap-4 lg:flex-row">
