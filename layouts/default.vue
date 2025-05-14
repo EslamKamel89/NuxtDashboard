2ddda8522e310b6bfe8694379ad93a30c46b0fb2
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-14 17:31:23 +0300

    Add Prettier configuration and integrate Tailwind CSS support

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="bg-blue-100 w-screen h-screen">
+    <slot />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1 class="block text-3xl font-bold text-red-500 underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
