e6fd27c6770320aa561332f3a30eb96e023bf2b7
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-14 17:35:52 +0300

    Enhance UI components with Tailwind CSS integration and add button variants

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts"></script>
 <template>
-  <div>
-    <h1 class="block text-3xl font-bold text-red-500 underline">
-      Hello world!
-    </h1>
-  </div>
+  <div></div>
 </template>
