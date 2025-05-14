e9541a5e0301137650885ae156faf8c323222d0f
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-15 00:23:49 +0300

    feat: update index page layout; enhance structure with header, main content, and footer sections

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="flex h-screen w-screen flex-col items-start lg:flex-row">
+  <div class="flex h-screen w-full flex-col items-start lg:flex-row">
     <Sidebar />
     <main class="h-full w-full flex-1 grow-1 p-8">
       <slot />
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts"></script>
 <template>
-  <div>
-    <div>
-      <header>
+  <div class="grid h-screen gap-8">
+    <header class="flex items-start justify-between">
+      <div class="shrink-0 grow">
+        <p>Hey Welcome back Eslam</p>
         <h1>Dashboard</h1>
-        <p>All information about your current finances.</p>
-      </header>
-      <main>
-        <div>Tabs : Today , week , Month , Year</div>
-        <section>Chart</section>
-      </main>
-      <footer>I'll have 3 different cards</footer>
-    </div>
+      </div>
+      <div class="h-[36px] w-[120px] bg-neutral-200"></div>
+    </header>
+    <main class="grid gap-2">
+      <div class="flex items-center gap-3">
+        <div
+          v-for="item in 3"
+          :key="item"
+          class="h-[36px] w-[120px] bg-neutral-200"
+        ></div>
+      </div>
+      <section>
+        <div class="h-[360px] w-full bg-neutral-200"></div>
+      </section>
+    </main>
+    <footer>
+      <div class="flex flex-col items-center gap-4 lg:flex-row">
+        <div
+          v-for="item in 3"
+          :key="item"
+          class="h-[260px] w-full bg-neutral-200"
+        ></div>
+      </div>
+    </footer>
   </div>
 </template>
